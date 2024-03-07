bf7ac702de101ae45e0649c46ffe937f798c9f22
augustin <augustin7766@gmail.com>
2024-03-07 20:24:15 +0530

    PrepareBaseModel

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 4. Update the entity
 5. Update the configuration manager in src config
 6. Update the components
-7. Update the pipeline
+7. Update the pipeline 
 8. Update the main.py
 <!-- dvc.yaml tracks the pipeline -->
 9. Update the dvc.yaml
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 from cnnClassifier import logger
 from ExceptionFile.exception import CustomException
 from cnnClassifier.pipeline.stage_01_DataIngestion import DataIngestionTrainingPipeline
-
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 
@@ -16,3 +16,15 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         raise CustomException(e , sys)
+
+
+
+STAGE_NAME = 'Prepare base Model'
+
+try:
+   logger.info(f'---------{STAGE_NAME} started')
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f'-------------{STAGE_NAME} completed----------------')
+except Exception as e:
+     raise CustomException(e, sys)
